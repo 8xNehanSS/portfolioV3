5d621a76a3c507a2a6c8647e1de46ac09161cb8d
8xNehanSS <nehan.sudasinghe@gmail.com>
2023-10-07 17:08:29 +0530

    Final adjustments of navbar

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,14 @@
+import "./herosection.css";
+
+const HeroSection = () => {
+  return (
+    <>
+      <div className="hero-spacing"></div>
+      <div className="hero">
+        <p className="blue hero-heading">Full Stack Developer</p>
+      </div>
+    </>
+  );
+};
+
+export default HeroSection;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,13 +2,15 @@ import "./navbar.css";
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav id="home">
       <div className="navbar">
         <p className="nav-heading">
           8x<span className="white">NehanSS</span>.dev
         </p>
         <ul className="nav-container">
-          <li className="nav-link">Home</li>
+          <li className="nav-link">
+            <a href="#home">Home</a>
+          </li>
           <li className="nav-link">About me</li>
           <li className="nav-link">Technologies</li>
           <li className="nav-link">Projects</li>

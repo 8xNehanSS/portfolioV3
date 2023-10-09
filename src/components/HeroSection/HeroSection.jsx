7c803675aedf59da261967b000c6c070ab641727
8xNehanSS <nehan.sudasinghe@gmail.com>
2023-10-09 15:25:10 +0530

    Animation of herosection complete

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,19 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./herosection.css";
 
 const HeroSection = () => {
   const text = ["Full Stack Developer", "Test1", "Test2"];
   const [textid, settextid] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      settextid((prevID) => {
+        let newID = prevID + 1;
+        if (newID === 3) {
+          newID = 0;
+        }
+        return newID;
+      });
+    }, 1000);
 
-  setInterval(() => {
-    settextid((prevID) => {
-      let newID = prevID + 1;
-      if (newID === 3) {
-        newID = 0;
-      }
-      return newID;
-    });
-  }, 4000);
+    return () => clearInterval(interval);
+  }, []);
+  console.log(textid);
   return (
     <>
       <div className="hero-spacing"></div>
